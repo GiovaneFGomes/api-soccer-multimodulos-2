a9c0aba3d51f7d89749a92117325132b53615897
Giovane <giovanefernandes35@gmail.com>
2021-12-06 11:14:19 -0300

    entity and repository

---
+++ src/main/java/com/giovane/soccer/entity/Team.java
@@ -0,0 +1,18 @@
+package com.giovane.soccer.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Document
+public class Team {
+    @Id
+    private String id;
+    private String name;
+    private String stadium;
+    private String country;
+}
---
+++ src/main/java/com/giovane/soccer/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.giovane.soccer.repository;
+
+import com.giovane.soccer.entity.Team;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface TeamRepository extends ReactiveMongoRepository <Team,String> {
+}
