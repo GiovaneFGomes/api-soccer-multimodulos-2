175bb967abacbf9f0d79a7e93a49a2ba72bd0151
Giovane <giovanefernandes35@gmail.com>
2022-01-11 11:46:43 -0300

    Exceptions reativas

--- src/main/java/com/giovane/soccer/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/giovane/soccer/exceptions/handler/GlobalExceptionHandler.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpServerErrorException.InternalServerError;
+import reactor.core.publisher.Mono;
+
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
@@ -21,42 +23,43 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(NOT_FOUND)
-    public ExceptionDetailsNotFound handlerNotFoundException(NotFoundException e){
-       return ExceptionDetailsNotFound.builder()
+    public Mono<ExceptionDetailsNotFound> handlerNotFoundException(NotFoundException e){
+        return Mono.just(ExceptionDetailsNotFound.builder()
                 .status(NOT_FOUND.value())
                 .title("Not found")
                 .timestamp(Instant.now())
                 .details(e.getMessage())
                 .developerMessage("Include a valid ID. Make sure it exists")
-                .build();
+                .build());
     }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(BAD_REQUEST)
-    public ExceptionMethodNotValidDetails handlerMethodNotValid(MethodArgumentNotValidException e){
+    public Mono<ExceptionMethodNotValidDetails> handlerMethodNotValid(MethodArgumentNotValidException e){
         Map<String, String> error = new HashMap<>();
         List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
         fieldErrors.forEach(p -> error.put(p.getField(), p.getDefaultMessage()));
 
-        return ExceptionMethodNotValidDetails.builder()
+        return Mono.just(ExceptionMethodNotValidDetails.builder()
                 .status(BAD_REQUEST.value())
                 .title("Body contains invalid JSON")
                 .timestamp(Instant.now())
                 .details(error)
                 .developerMessage("Error! Check the body constraints")
-                .build();
+                .build());
     }
 
     @ExceptionHandler(InternalServerError.class)
     @ResponseStatus(INTERNAL_SERVER_ERROR)
-    public ExceptionDetailsInternalError handlerInternalServerError(){
-       return ExceptionDetailsInternalError.builder()
+    public Mono<ExceptionDetailsInternalError> handlerInternalServerError(){
+       return Mono.just(ExceptionDetailsInternalError.builder()
                 .status(INTERNAL_SERVER_ERROR.value())
                 .title("Internal Server Error")
                 .timestamp(Instant.now())
                 .details("Something went wrong")
                 .developerMessage("Check the request")
-                .build();
+                .build());
     }
 
 }
